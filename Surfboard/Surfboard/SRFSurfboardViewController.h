deee55b62d656757f39fe9fcd09e5624bd2505bd
Moshe Berman <moshefromsinai@gmail.com>
2014-08-12 18:51:14 -0400

    Implemented the basic panels view controller.

--- Surfboard/Surfboard/SRFSurfboardViewController.h
+++ Surfboard/Surfboard/SRFSurfboardViewController.h
@@ -8,6 +8,58 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MBSurfboardViewController : UIViewController
+@interface SRFSurfboardViewController : UICollectionViewController
+
+#pragma mark - Initializers
+
+/**
+ *  Instantiates a new surfboard with an array of panel objects.
+ *
+ *  @param configuration An array of panels.
+ *  @return An SRFSurfboardViewController.
+ */
+
+- (instancetype)initWithPathToConfiguration:(NSString *)path;
+
+/**
+ *  Instantiates a new surfboard with an array of panel objects.
+ *
+ *  @param panels An array of panels.
+ *  @return An SRFSurfboardViewController
+ */
+
+- (instancetype)initWithPanels:(NSArray *)panels;
+
+#pragma mark - Setting the Panels After Initializing
+
+/** ---
+ *  @name Setting the Panels After Initializing
+ *  ---
+ */
+
+/**
+ *  Sets the panels and reloads the surfboard.
+ *
+ *  @param panels An array of panels.
+ */
+
+- (void)setPanels:(NSArray *)panels;
+
+#pragma mark - Load Panels from Configuration
+
+/** ---
+ *  @name Load Panels From Configuration
+ *  ---
+ */
+
+/**
+ *  Loads panels out from a given path.
+ *
+ *  @param path The path from which to load the panel configuration.
+ *
+ *  @return An array of panels.
+ */
+
++ (NSArray *)panelsFromConfigurationAtPath:(NSString *)path;
 
 @end
--- Surfboard/Surfboard/SRFSurfboardViewController.m
+++ Surfboard/Surfboard/SRFSurfboardViewController.m
@@ -6,8 +6,221 @@
 //  Copyright (c) 2014 Moshe Berman. All rights reserved.
 //
 
-#import "MBSurfboardViewController.h"
+#import "SRFSurfboardViewController.h"
+#import "SRFSurfboardPanelCell.h"   //  the panel cell
+#import "SRFSurfboardPanel.h"
 
-@implementation MBSurfboardViewController
+/**
+ *  An identifier for surfboard cells.
+ */
+
+static NSString *kSurfboardPanelIdentifier = @"com.mosheberman.surfboard-panel";
+
+/**
+ *  An extension of the surfbord view controller.
+ */
+
+@interface SRFSurfboardViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+/**
+ *  An array of configuration elements.
+ */
+
+@property (nonatomic, strong) NSArray *panels;
+
+@end
+
+@implementation SRFSurfboardViewController
+
+#pragma mark - Initializers
+
+/** ---
+ *  @name Initializers
+ *  ---
+ */
+
+/**
+ *  Instantiates a new surfboard with an array of panel objects.
+ *
+ *  @param configuration An array of panels.
+ *  @return An SRFSurfboardViewController.
+ */
+
+- (instancetype)initWithPathToConfiguration:(NSString *)path
+{
+
+    NSArray *panels = [SRFSurfboardViewController panelsFromConfigurationAtPath:path];
+    
+    self = [self initWithPanels:panels];
+    
+    if (self)
+    {
+        
+    }
+    
+    return self;
+}
+
+/**
+ *  Instantiates a new surfboard with an array of panel objects.
+ *
+ *  @param panels An array of panels.
+ *  @return An SRFSurfboardViewController.
+ */
+
+- (instancetype)initWithPanels:(NSArray *)panels
+{
+    self = [super initWithCollectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+    
+    if (self)
+    {
+        _panels = panels;
+    }
+    
+    return self;
+}
+
+#pragma mark - View Lifecycle
+
+/** ---
+ *  @name View Lifecycle
+ *  ---
+ */
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    /**
+     *  Configure the layout.
+     */
+    
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *)self.collectionViewLayout;
+    
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    /**
+     *  Register a nib for the surfboard panel class.
+     */
+    
+    UINib *nib = [UINib nibWithNibName:@"SRFSurfboardPanelCell" bundle:nil];
+    [self.collectionView registerNib:nib forCellWithReuseIdentifier:kSurfboardPanelIdentifier];
+    
+    /**
+     *  Wire up the delegate and data source.
+     */
+    
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+}
+
+#pragma mark - UICollectionViewDataSource
+
+/** ---
+ *  @name UICollectionViewDataSource
+ *  ---
+ */
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    NSInteger count = self.panels.count;
+    
+    return count;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    SRFSurfboardPanelCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kSurfboardPanelIdentifier forIndexPath:indexPath];
+    
+    //  TODO: Apply panel contents here.
+    
+    if (indexPath.row > 0 && indexPath.row < self.panels.count)
+    {
+        SRFSurfboardPanel *panel = self.panels[indexPath.row];
+        
+        cell.textView.text = panel.text;
+        cell.imageView.image = panel.image;
+        
+        [cell.actionButton setTitle:panel.buttonTitle forState:UIControlStateNormal];
+
+        //  Hide the button on panels with no title.
+        cell.actionButton.hidden = (panel.buttonTitle == nil);
+    }
+    
+    return cell;
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+
+/** ---
+ *  @name UICollectionViewDelegateFlowLayout
+ *  ---
+ */
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return self.collectionView.bounds.size;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 0.0f;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 0.0f;
+}
+
+#pragma mark - Setters
+
+/** ---
+ *  @name Setters
+ *  ---
+ */
+
+/**
+ *  Sets the panels and reloads the surfboard.
+ *
+ *  @param panels An array of panels.
+ */
+
+- (void)setPanels:(NSArray *)panels
+{
+    _panels = panels;
+    [[self collectionView] reloadSections:[NSIndexSet indexSetWithIndex:0]];
+}
+
+
+#pragma mark - Load Panels from Configuration
+
+/** ---
+ *  @name Load Panels From Configuration
+ *  ---
+ */
+
+/**
+ *  Loads panels out from a given path.
+ *
+ *  @param path The path from which to load the panel configuration.
+ *
+ *  @return An array of panels.
+ */
+
++ (NSArray *)panelsFromConfigurationAtPath:(NSString *)path
+{
+    NSData *panelData = [[NSData alloc] initWithContentsOfFile:path];
+    
+    NSArray *panels = nil;
+    
+    if (panelData)
+    {
+        panels = [NSJSONSerialization JSONObjectWithData:panelData options:0 error:nil];
+    }
+    
+    return panels;
+}
 
 @end
