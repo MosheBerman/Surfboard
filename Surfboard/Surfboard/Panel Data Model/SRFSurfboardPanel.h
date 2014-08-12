28011dcc0df4b1ae441c1607c5ddd37b6d2d55e0
Moshe Berman <moshefromsinai@gmail.com>
2014-08-12 16:18:27 -0400

    Created a data model class.

---
+++ Surfboard/Surfboard/Panel Data Model/SRFSurfboardPanel.h
@@ -0,0 +1,47 @@
+//
+//  SRFSurfboardPanel.h
+//  Surfboard
+//
+//  Created by Moshe on 8/12/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  A panel type enum - for future use.
+ */
+
+NS_ENUM(NSInteger, SRFSurfboardPanelType)
+{
+    SRFSurfboardPanelDefault = 0    // A panel with a block of text at the top, an image, and an optional button.
+};
+
+@interface SRFSurfboardPanel : NSObject
+
+/**
+ *  The text to display in the panel.
+ */
+
+@property (nonatomic, strong) NSString *text;
+
+/**
+ *  An image to load into the panel.
+ *
+ *  For the default panel, the dimensions of this 
+ *  image should be 141 points wide and 249 points 
+ *  tall, or a multiple of that ratio.
+ */
+
+@property (nonatomic, strong) UIImage *image;
+
+/**
+ *  A title for a button.
+ *
+ *  If this is set to nil, the panel cell will 
+ *  hide its button.
+ */
+
+@property (nonatomic, strong) NSString *buttonTitle;
+
+@end
---
+++ Surfboard/Surfboard/Panel Data Model/SRFSurfboardPanel.m
@@ -0,0 +1,13 @@
+//
+//  SRFSurfboardPanel.m
+//  Surfboard
+//
+//  Created by Moshe on 8/12/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import "SRFSurfboardPanel.h"
+
+@implementation SRFSurfboardPanel
+
+@end
