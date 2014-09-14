d15ad42c22744cc7ccaa5694f6bed5e694d66965
Abraham Vegh <me@abrahamvegh.com>
2014-09-14 15:29:24 -0400

    Add NS_DESIGNATED_INITIALIZER for older versions of Xcode where it is undefined.

--- Surfboard/SRFSurfboard/Surfboard View Controller/SRFSurfboardViewController.h
+++ Surfboard/SRFSurfboard/Surfboard View Controller/SRFSurfboardViewController.h
@@ -57,6 +57,14 @@
  *  @return An SRFSurfboardViewController
  */
 
+#ifndef NS_DESIGNATED_INITIALIZER
+#if __has_attribute(objc_designated_initializer)
+#define NS_DESIGNATED_INITIALIZER __attribute__((objc_designated_initializer))
+#else
+#define NS_DESIGNATED_INITIALIZER
+#endif
+#endif
+
 - (instancetype)initWithPanels:(NSArray *)panels NS_DESIGNATED_INITIALIZER;
 
 #pragma mark - Setting the Panels After Initializing
