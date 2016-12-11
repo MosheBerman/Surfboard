81959054fcfdbc59a4832d59bde6761016097a36
Moshe Berman <moshefromsinai@gmail.com>
2016-12-11 11:15:28 -0500

    Factored the surfboard panel type enum out into its own header.

--- Surfboard/SRFSurfboard/Panel Data Model/SRFSurfboardPanel.h
+++ Surfboard/SRFSurfboard/Panel Data Model/SRFSurfboardPanel.h
@@ -8,15 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-/**
- *  A panel type enum - for future use.
- */
-
-NS_ENUM(NSInteger, SRFSurfboardPanelType)
-{
-    SRFSurfboardPanelDefault = 0    // A panel with a block of text at the top, an image, and an optional button.
-};
-
 @interface SRFSurfboardPanel : NSObject
 
 /**
---
+++ Surfboard/SRFSurfboard/Panel Data Model/SRFSurfboardPanelType.h
@@ -0,0 +1,22 @@
+//
+//  SRFSurfboardPanelType.h
+//  Surfboard
+//
+//  Created by Moshe Berman on 12/11/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+#ifndef SRFSurfboardPanelType_h
+#define SRFSurfboardPanelType_h
+
+/**
+ *  A panel type enum - for future use.
+ */
+
+NS_ENUM(NSInteger, SRFSurfboardPanelType)
+{
+    SRFSurfboardPanelDefault = 0    // A panel with a block of text at the top, an image, and an optional button.
+};
+
+
+#endif /* SRFSurfboardPanelType_h */
